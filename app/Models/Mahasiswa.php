ab5d106866316e80a8426894184adae19e1c4546
unknown <zaizhamichella09@gmail.com>
2024-04-08 00:20:53 +0700

    first commit

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Mahasiswa extends Model
 {
     use HasFactory;
+
+    protected $table = 'mahasiswa';
+    protected $primaryKey = 'id_mahasiswa';
+
+    public function ktm(){
+        return $this->hasOne(Ktm::class, 'id_mahasiswa');
+    }
 }
